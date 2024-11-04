965a61c1323f512329069814f9776d7a1b23856b
Eric M <ern.martin12@gmail.com>
2024-11-04 17:23:33 -0500

    added script that uses images instead of webcam. also added images used for confusion matrix

--- face_detection/face_detection.py
+++ face_detection/face_detection.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
-faceCascade = cv2.CascadeClassifier('face_detection/haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture(0)
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(1)
 cap.set(3,640) # set Width
 cap.set(4,480) # set Height
 while True:
